7edc46e93c6d3f751728b6b4688a074fd409f62b
Dinesh Sawant <disawant@in.ibm.com>
2018-11-12 19:28:55 +0530

    Add sort logic on cards by suit

--- app.js
+++ app.js
@@ -9,12 +9,27 @@ angular
     self.drawCards = [];
 
     self.createDeck = function() {
-        this.names = ['A', '2', '3', '4', '5', '6', '7', '8', '9', '10', 'J', 'Q', 'K'];
+        this.names = [
+            {index: 0, value: 'A'}, 
+            {index: 1, value: '2'},
+            {index: 2, value: '3'}, 
+            {index: 3, value: '4'}, 
+            {index: 4, value: '5'},
+            {index: 5, value: '6'}, 
+            {index: 6, value: '7'}, 
+            {index: 7, value: '8'}, 
+            {index: 8, value: '9'}, 
+            {index: 9, value: '10'}, 
+            {index: 10, value: 'J'}, 
+            {index: 11, value: 'Q'}, 
+            {index: 12, value: 'K'}
+        ];
+
         this.suits = [
-            {'icon': '\u2663', 'color': 'black'},
-            {'icon': '\u2660', 'color': 'black'},
-            {'icon': '\u2661', 'color': 'red'},
-            {'icon': '\u2662', 'color': 'red'}
+            {'name': 'Clubs', 'icon': '\u2663', 'color': 'black'},
+            {'name': 'Spades', 'icon': '\u2660', 'color': 'black'},
+            {'name': 'Hearts', 'icon': '\u2661', 'color': 'red'},
+            {'name': 'Diamonds', 'icon': '\u2662', 'color': 'red'}
         ];
 
         for( var s = 0; s < this.suits.length; s++ ) {
@@ -40,7 +55,42 @@ angular
     }
 
     self.sortCards = function() {
-        // self.drawCards = [];
-        // self.drawCards = self.createDeck();
+        var clubs = [], spades = [], hearts = [], diamonds = [];
+
+        for( var i = 0; i < self.drawCards.length; i++ ) {
+            
+            if(self.drawCards[i].suit.name == 'Clubs') {
+                clubs.push(self.drawCards[i]);
+            }
+            if(self.drawCards[i].suit.name == 'Spades') {
+                spades.push(self.drawCards[i]);
+            }
+            if(self.drawCards[i].suit.name == 'Hearts') {
+                hearts.push(self.drawCards[i]);
+            }
+            if(self.drawCards[i].suit.name == 'Diamonds') {
+                diamonds.push(self.drawCards[i]);
+            }
+        }
+
+        clubs = self.sortOneSuit(clubs);
+        spades = self.sortOneSuit(spades);
+        spades = self.sortOneSuit(spades);
+        diamonds = self.sortOneSuit(diamonds);
+
+        self.drawCards = clubs.concat(spades, hearts, diamonds);
+    }
+
+    self.sortOneSuit = function(suit) {
+        var temp = suit.sort(function(a, b) {
+            if (b.value.index < a.value.index )
+                return 1;
+            else if (b.value.index > a.value.index)
+                return -1;
+            else 
+                return 0;
+        });
+
+        return temp;
     }
   }
